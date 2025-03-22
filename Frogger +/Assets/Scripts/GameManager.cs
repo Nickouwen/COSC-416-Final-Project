384b070a0672b6e6b3838831fdd42d9cf8900471
Ian D <ianmd72@gmail.com>
2025-03-22 16:50:35 -0700

    moving boat added to level 1 scene

--- Frogger +/Assets/Scripts/GameManager.cs
+++ Frogger +/Assets/Scripts/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
     public GameObject boatPrefab;
     public Transform boatSpawn;
     public Transform playerSpawn;
+    public GameObject player;
+    public GameObject leftWall;
+    public GameObject rightWall;
 
     public static GameManager Instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -38,10 +41,13 @@ public class GameManager : MonoBehaviour
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 
-    public static void SpawnPlatform(GameObject platformPrefab, Transform spawnPoint, float velocity)
+    public void SpawnPlatform(GameObject platformPrefab, Transform spawnPoint, float velocity)
     {
         GameObject platform = Instantiate(platformPrefab, spawnPoint.position, spawnPoint.rotation);
-        platform.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, velocity);
+        // ignore collisions with the walls only
+        Physics.IgnoreCollision(leftWall.GetComponent<Collider>(), platform.GetComponent<Collider>());
+        Physics.IgnoreCollision(rightWall.GetComponent<Collider>(), platform.GetComponent<Collider>());
+        platform.GetComponent<Rigidbody>().linearVelocity = new Vector3(velocity, 0, 0);
         Destroy(platform, 10);
     }
 }
