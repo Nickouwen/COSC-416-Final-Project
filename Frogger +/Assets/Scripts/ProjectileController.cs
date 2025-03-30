768bb26fbcce710c13046cfb7c54ee1bd288069b
Nickouwen <71842732+Nickouwen@users.noreply.github.com>
2025-03-30 14:15:48 -0700

    Added roads and islands; updated turrets

--- Frogger +/Assets/Scripts/ProjectileController.cs
+++ Frogger +/Assets/Scripts/ProjectileController.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    public float spinSpeed = 90f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, spinSpeed/2 *  Time.deltaTime);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Shield"))
